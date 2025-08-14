ab405e0f269ce22f2fa8e100b274ef142af6959e
Yvanik <kayvin4@hotmail.com>
2025-08-14 17:37:39 +0200

    fix: style

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import OrderForm from './components/order-form'
 function App() {
   return (
     <>
-      <div>
-        <h1 className="text-3xl font-bold underline">Hyperliquid Order APP</h1>
+      <div className='flex flex-col items-center'>
+        <h1 className="mb-4 text-3xl font-bold underline">Hyperliquid Order APP</h1>
           <OrderForm />
       </div>
     </>
